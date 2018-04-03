e42613e36822bfb41eb9d80e43a8dd0ded3313ef
Juraj Hamornik <jurko@bdi.sk>
2018-04-03 23:57:42 +0000

    Merged PR 13: Merge devel to master
    Related work items: #53

--- smartHookah/Models/ChangeNameViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace smartHookah.Models
-{
-    public class ChangeNameViewModel
-    {
-        public string Name { get; set; }
-    }
-}
---
+++ smartHookah/Models/ViewModel/SmokeSessionListItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace smartHookah.Models
+{
+    public class SmokeSessionListItemViewModel 
+    {
+        public SmokeSession SmokeSession { get; set; }
+
+        public int? OverallReview { get; set; }
+    }
+}
