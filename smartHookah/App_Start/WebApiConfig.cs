9e874f30b73f436258a45962dac7198f5ff4b8fa
Juraj Hamornik <jurko@bdi.sk>
2018-04-04 15:31:54 +0000

    Merged PR 21: ping

--- smartHookah/App_Start/WebApiConfig.cs
+++ smartHookah/App_Start/WebApiConfig.cs
@@ -15,11 +15,11 @@ namespace smartHookah
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "ActionApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
-                defaults:new {id = RouteParameter.Optional}
-            );
+            //config.Routes.MapHttpRoute(
+            //    name: "ActionApi",
+            //    routeTemplate: "api/{controller}/{action}/{id}",
+            //    defaults:new {id = RouteParameter.Optional}
+            //);
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
