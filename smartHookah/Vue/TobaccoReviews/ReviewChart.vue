c6219592d3caa59f9bf47152f9640e2ff284157e
Miloš Hampl <miloshampl@hotmail.cz>
2018-05-30 09:17:06 +0200

    vue reviews rozdelane

---
+++ smartHookah/Vue/TobaccoReviews/ReviewChart.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="review-div" id="reviewChartDiv" style="position:relative">
+    <canvas class="review-chart" id="reviewChart"></canvas>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'review-chart',
+    props: '[Smoke, Taste, Quality, Overall]',
+    data: () => {
+      return {
+        radarChart: '',
+        chartOptions: {
+          datalabels: {
+            display: false
+          },
+          legend: {
+            display: false
+          },
+          scale: {
+            ticks: {
+              beginAtZero: true,
+              min: 0,
+              max: 10,
+              stepSize: 1
+            },
+            pointLabels: {
+              fontSize: 16
+            }
+          }
+        },
+        datasetLabels: [
+          "Session Quality",
+          "Tobacco Taste",
+          "Tobacco Quality",
+          "Overall Rating"
+        ]
+      }
+    },
+    methods: {
+      drawChart: function () {
+        var chartData = {
+          labels: [
+            "Smoke Quality",
+            "Tobacco Taste",
+            "Tobacco Quality",
+            "Overall Rating"
+          ],
+          datasets: [
+            {
+              label: "Rating",
+              backgroundColor: "transparent",
+              borderColor: "#3f51b5",
+              fill: false,
+              radius: 5,
+              pointRadius: 5,
+              pointBorderWidth: 3,
+              pointBackgroundColor: "#3f51b5",
+              pointBorderColor: "#3f51b5",
+              pointHoverRadius: 6,
+              data: [
+                this.review.Smoke,
+                this.review.Taste,
+                this.review.Quality,
+                this.review.Overall
+              ]
+            }
+          ]
+        };
+        $("canvas#reviewChart").remove();
+        $("div#reviewChartDiv").append('<canvas id="reviewChart"></canvas>');
+        this.radarChart = new Chart($("#reviewChart"),
+          {
+            type: 'radar',
+            data: chartData,
+            options: this.chartOptions
+          });
+      }
+    }
+  }
+</script>
---
+++ smartHookah/Vue/TobaccoReviews/TobaccoReview.vue
@@ -0,0 +1,164 @@
+<template>
+  <div v-show="tobaccoFilled" id="tobaccoReview" class="form-horizontal" style="margin-left: auto; margin-right: auto; max-width: 40em">
+
+    <review-chart />
+
+    <div v-show="!edit" class="review-div" id="reviewSliders">
+      <fieldset class="form-group">
+        <div class="bmd-label-static">@LocalizationHelper.Translate("_TobaccoRevSmokeQ", "SmokeSession")&nbsp;-&nbsp;<h6 style="display: inline; font-weight: bold">{{review.Smoke + '/10'}}</h6></div>
+        <div class="form-control">
+          <input class="col-md-12 review-slider" type="range" min="0" max="10" step="1" id="Smoke" v-bind:disabled="edit" v-model="review.Smoke" />
+        </div>
+      </fieldset>
+
+      <div class="form-group">
+        <div class="bmd-label-static">@LocalizationHelper.Translate("_TobaccoRevTobTaste", "SmokeSession")&nbsp;-&nbsp;<h6 style="display: inline; font-weight: bold">{{review.Taste + '/10'}}</h6></div>
+        <div class="form-control">
+          <input class="col-md-12 review-slider" type="range" min="0" max="10" step="1" id="Taste" v-bind:disabled="edit" v-model="review.Taste" />
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="bmd-label-static">@LocalizationHelper.Translate("_TobaccoRevTobaccoQ", "SmokeSession")&nbsp;-&nbsp;<h6 style="display: inline; font-weight: bold">{{review.Quality + '/10'}}</h6></div>
+        <div class="form-control">
+          <input class="col-md-12 review-slider" type="range" min="0" max="10" step="1" id="Quality" v-bind:disabled="edit" v-model="review.Quality" />
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="bmd-label-static">@LocalizationHelper.Translate("_TobaccoRevOverallRating", "SmokeSession")&nbsp;-&nbsp;<h6 style="display: inline; font-weight: bold">{{review.Overall + '/10'}}</h6></div>
+        <div class="form-control">
+          <input class="col-md-12 review-slider" type="range" min="0" max="10" step="1" id="Rating" v-bind:disabled="edit" v-model="review.Overall" />
+        </div>
+      </div>
+    </div>
+
+
+    <div class="form-group">
+      <label class="bmd-label-static">@LocalizationHelper.Translate("_TobaccoRevTextReview", "SmokeSession")</label>
+      <div>
+        <textarea class="col-md-12 review-text form-control" id="Text" v-bind:disabled="edit" v-model="review.Text"></textarea>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <button v-show="edit" class="btn btn-primary btn-raised" v-bind:disabled="!edit" v-on:click="edit = !edit">@LocalizationHelper.Translate("Edit", "Common")</button>
+
+      <button v-show="!edit" class="btn btn-danger btn-raised" v-bind:disabled="edit" v-on:click="cancelReview()">@LocalizationHelper.Translate("Cancel", "Common")</button>
+
+      <button class="btn btn-success btn-raised" style="float: right" v-bind:disabled="edit" v-on:click="saveReview()">@LocalizationHelper.Translate("SaveChanges", "Common")</button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+    export default {
+    name: 'tobacco-review',
+    props: '[sessionId]',
+    data: () => {
+      return {
+        review: {
+          Id: '',
+          Quality: 0,
+          Taste: 0,
+          Smoke: 0,
+          Overall: 0,
+          Text: '',
+          SmokeSessionId: ''
+        },
+        edit: false,
+        tobaccoFilled: true
+      }
+    },
+
+    created: function() {
+
+      $.getJSON('/TobaccoReviews/GetReviewVue/',
+        {
+          sessionId: this.getSessionId()
+        },
+        function(data) {
+          if (data != null) {
+            vm.$data.review = data;
+            vm.$data.edit = true;
+            vm.$data.tobaccoFilled = true;
+            vm.drawChart();
+            if (getTextLines() < 11) {
+              $("#Text").animate({ rows: getTextLines() }, 'fast');
+            } else {
+              $("#Text").animate({ rows: 10 }, 'fast');
+            }
+          }
+        });
+    },
+
+    methods: {
+      cancelReview: function() {
+        $.getJSON('/TobaccoReviews/GetReviewVue/',
+          {
+            sessionId: this.getSessionId()
+          },
+          function(data) {
+            if (data != null) {
+              vm.$data.review = data;
+              vm.$data.reviewOld = data;
+              vm.$data.edit = true;
+              if (getTextLines() < 11) {
+                $("#Text").animate({ rows: getTextLines() }, 'fast');
+              } else {
+                $("#Text").animate({ rows: 10 }, 'fast');
+              }
+            }
+          });
+      },
+
+      saveReview: function() {
+        this.review.SmokeSessionId = this.getSessionId();
+
+        var jqxhr = $.ajax({
+          type: "POST",
+          url: "/TobaccoReviews/SaveVueReview/",
+          data: JSON.stringify({
+            Id: this.review.Id,
+            Quality: this.review.Quality,
+            Taste: this.review.Taste,
+            Smoke: this.review.Smoke,
+            Overall: this.review.Overall,
+            Text: this.review.Text,
+            SmokeSessionId: this.review.SmokeSessionId
+          }),
+          contentType: "application/json; charset=utf-8",
+          dataType: "json",
+          success: function(data) {
+            vm.$data.review = data;
+            vm.$data.edit = true;
+            vm.drawChart();
+          }
+        }).fail(function(jqXHR, exception) {
+          var msg = '';
+          if (jqXHR.status === 0) {
+            msg = 'Not connect.\n Verify Network.';
+          } else if (jqXHR.status == 404) {
+            msg = 'Requested page not found. [404]';
+          } else if (jqXHR.status == 500) {
+            msg = 'Internal Server Error [500].';
+          } else if (exception === 'parsererror') {
+            msg = 'Requested JSON parse failed.';
+          } else if (exception === 'timeout') {
+            msg = 'Time out error.';
+          } else if (exception === 'abort') {
+            msg = 'Ajax request aborted.';
+          } else {
+            msg = 'Uncaught Error.\n' + jqXHR.responseText;
+          }
+          console.log(msg);
+        });
+      },
+
+      getSessionId: function() {
+        return $("input[name='smokeSessionId']").val();
+      }
+    }
+  };
+</script>
