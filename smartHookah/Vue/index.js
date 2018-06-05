3c4552f0bcdab98fb87d7a3a16847beeed3b0e96
Miloš Hampl <miloshampl@hotmail.cz>
2018-06-05 10:03:43 +0200

    vue.js reviews in progress

--- smartHookah/Vue/index.js
+++ smartHookah/Vue/index.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import moment  from 'moment'
 
 import reservation_place from './Reservations/ReservationPlace.vue'
+import tobacco_review from './TobaccoReviews/TobaccoReview.vue'
+import review_chart from './TobaccoReviews/ReviewChart.vue'
 
 Vue.component('reservation_place', reservation_place);
+Vue.component('tobacco_review', tobacco_review);
+Vue.component('review_chart', review_chart);
 
 
 var v = Vue;
