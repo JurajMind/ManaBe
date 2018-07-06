1052fce76e4046a08f045b346f6ea4aeb08e38c0
Juraj Hamornik <jurko@bdi.sk>
2018-07-06 16:52:32 +0000

    Merged PR 44: gdpr
    gdpr

---
+++ smartHookah/Controllers/AssetsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace smartHookah.Controllers
+{
+    public class AssetsController : Controller
+    {
+        [Route("~/PrivacyPolicy")]
+        // GET: Assets
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+        public ActionResult TermOfUssage()
+        {
+            return View();
+        }
+    }
+}
