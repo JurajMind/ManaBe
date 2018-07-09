29695b77fc91aa257c25f3d565b37e4466bbfd1d
Miloš Hampl <miloshampl@hotmail.cz>
2018-07-09 15:12:31 +0200

    Featured mix creators a presun Brand do samostatneho souboru

---
+++ smartHookah/Models/Db/Brand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace smartHookah.Models.Db
+{
+    public class Brand
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Picture { get; set; }
+
+        public bool Tobacco { get; set; }
+
+        public bool Bowl { get; set; }
+
+        public bool Hookah { get; set; }
+
+        public bool TobaccoMixBrand { get; set; }
+
+        public virtual ICollection<PipeAccesory> PipeAccesories { get; set; }
+
+        public string DisplayName { get; set; }
+
+    }
+}
---
+++ smartHookah/Models/Dto/MixologyDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace smartHookah.Models.Dto
+{
+    public class MixCreatorsDTO : DTO
+    {
+        public ICollection<MixCreator> MixCreatorsList { get; set; }
+
+        public MixCreatorsDTO()
+        {
+            this.MixCreatorsList = new List<MixCreator>();
+        }
+    }
+
+    public class MixCreator
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Picture { get; set; }
+        public int MixCount { get; set; }
+    }
+}
