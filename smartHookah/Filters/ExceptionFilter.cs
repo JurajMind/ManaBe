c8255396bcfceba316b5cc9c4f6e3f8428e3be5a
Juraj Hamornik <hamornik@manapipes.com>
2018-07-11 01:13:16 +0200

    403

---
+++ smartHookah/Filters/ExceptionFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace smartHookah.Filters
+{
+    using System.Web.Http.Filters;
+
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+    }
+}
