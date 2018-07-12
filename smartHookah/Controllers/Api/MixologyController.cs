43d86bf6e2787e3bdb6084c82fdc684814bbd7dd
Miloš Hampl <miloshampl@hotmail.cz>
2018-07-12 17:01:34 +0200

    add mix

--- smartHookah/Controllers/Api/MixologyController.cs
+++ smartHookah/Controllers/Api/MixologyController.cs
@@ -3,10 +3,13 @@ using smartHookah.Models;
 using smartHookah.Models.Dto;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
+using Tobacco = smartHookah.Models.Dto.Tobacco;
 
 namespace smartHookah.Controllers.Api
 {
@@ -27,7 +30,7 @@ namespace smartHookah.Controllers.Api
         public async Task<MixListDTO> GetMixes(int page = 0, int pageSize = 50, string author = "me", string orderBy = "name", string order = "asc")
         {
             var query = from a in _db.TobaccoMixs select a;
-            if (_db.Brands.Where(a => a.TobaccoMixBrand == true && a.Name.ToLower() == author.ToLower()).Count() > 0)
+            if (_db.Brands.Any(a => a.TobaccoMixBrand && a.Name.ToLower() == author.ToLower()))
             {
                 query = from m in query where m.Brand.Name.ToLower() == author.ToLower() select m;
             } else if (author == "me")
@@ -74,8 +77,8 @@ namespace smartHookah.Controllers.Api
                     {
                         Id = r.Id,
                         AccName = r.AccName,
-                        Overall = r.Statistics != null ? r.Statistics.Overall : -1,
-                        Used = r.Statistics != null ? r.Statistics.Used : -1
+                        Overall = r.Statistics?.Overall ?? -1,
+                        Used = r.Statistics?.Used ?? -1
                     };
                     foreach (var x in r.Tobaccos)
                     {
@@ -110,7 +113,7 @@ namespace smartHookah.Controllers.Api
                     query = order.ToLower() == "asc" ? from a in query orderby a.DisplayName ascending select a : from a in query orderby a.DisplayName descending select a;
                     break;
                 case "count":
-                    query = order.ToLower() == "asc" ? from a in query orderby a.PipeAccesories.Where(x => x is TobaccoMix).Count() ascending select a : from a in query orderby a.PipeAccesories.Where(x => x is TobaccoMix).Count() descending select a;
+                    query = order.ToLower() == "asc" ? from a in query orderby a.PipeAccesories.Count(x => x is TobaccoMix) ascending select a : from a in query orderby a.PipeAccesories.Count(x => x is TobaccoMix) descending select a;
                     break;
                 default:
                     return new MixCreatorsDTO() { Success = false, Message = "Invalid OrderBy value, select \"name\" or \"count\"." };
@@ -129,7 +132,7 @@ namespace smartHookah.Controllers.Api
                         Name = m.Name,
                         DisplayName = m.DisplayName,
                         Picture = m.Picture,
-                        MixCount = m.PipeAccesories.Where(a => a is TobaccoMix).Count()
+                        MixCount = m.PipeAccesories.Count(a => a is TobaccoMix)
                     };
 
                     result.MixCreatorsList.Add(creator);
@@ -139,6 +142,102 @@ namespace smartHookah.Controllers.Api
 
             return new MixCreatorsDTO() { Success = false, Message = "No mix creators found." };
         }
+        #endregion
+
+        #region Setters
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("AddToMix")]
+        public async Task<TobaccoMixDTO> AddToMix([Bind(Include = "Id,Fraction,MixId,AccName")] MixPartDTO mixPart)
+        {
+            var tobacco = _db.Tobaccos.Find(mixPart.Id);
+
+            if (mixPart.Fraction < 1 || mixPart.Fraction > 40 || tobacco == null)
+            {
+                return new TobaccoMixDTO(){Success = false, Message = "Tobacco not fount or fraction not within acceptable range."};
+            }
+
+            var mix = _db.TobaccoMixs.Find(mixPart.MixId);
+            if (mix != null)
+            {
+                var t = new TobacoMixPart()
+                {
+                    TobaccoId = mixPart.Id,
+                    Fraction = mixPart.Fraction
+                };
+                if (mix.Tobaccos.Any(a => a.TobaccoId == mixPart.Id)) return new TobaccoMixDTO(){Success = false, Message = "This tobacco was already added to mix. "};
+                try
+                {
+                    mix.Tobaccos.Add(t);
+                    _db.TobaccoMixs.AddOrUpdate(mix);
+                    _db.SaveChanges();
+                    var response = new TobaccoMixDTO()
+                    {
+                        Success = true,
+                        Message = $"Tobacco with id {mixPart.Id} added to mix.",
+                        Id = mix.Id,
+                        AccName = mix.AccName
+                    };
+                    foreach (var m in mix.Tobaccos)
+                    {
+                        var x = new Tobacco()
+                        {
+                            Id = m.Tobacco.Id,
+                            Fraction = m.Fraction,
+                            AccName = m.Tobacco.AccName,
+                            BrandName = m.Tobacco.BrandName
+                        };
+                        response.Tobaccos.Add(x);
+                    }
+                    return response;
+                }
+                catch(Exception e)
+                {
+                    return new TobaccoMixDTO(){Success = false, Message = e.Message};
+                }
+            }
+
+            var author = UserHelper.GetCurentPerson(_db);
+            mix = new TobaccoMix()
+            {
+                AccName = mixPart.AccName,
+                Author = author,
+                CreatedAt = DateTimeOffset.UtcNow,
+                BrandName = author.AssignedBrandId ?? "OwnBrand"
+            };
+            if (mix.Tobaccos.Any(a => a.TobaccoId == mixPart.Id)) return new TobaccoMixDTO() { Success = false, Message = "This tobacco was already added to mix. " };
+            mix.Tobaccos.Add(new TobacoMixPart(){TobaccoId = mixPart.Id, Fraction = mixPart.Fraction});
+
+            try
+            {
+                _db.TobaccoMixs.AddOrUpdate(mix);
+                _db.SaveChanges();
+                var response = new TobaccoMixDTO()
+                {
+                    Success = true,
+                    Message = $"Tobacco with id {mixPart.Id} added to new mix.",
+                    Id = mix.Id,
+                    AccName = mix.AccName
+                };
+                foreach (var m in mix.Tobaccos)
+                {
+                    var x = new Tobacco()
+                    {
+                        Id = m.Tobacco.Id,
+                        Fraction = m.Fraction,
+                        AccName = m.Tobacco.AccName,
+                        BrandName = m.Tobacco.BrandName
+                    };
+                    response.Tobaccos.Add(x);
+                }
+                return response;
+            }
+            catch (Exception e)
+            {
+                return new TobaccoMixDTO(){Success = false, Message = e.Message};
+            }
+        }
+
         #endregion
     }
 }
