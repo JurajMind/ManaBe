586b63a1a621618ca91937815536f61e62c41619
Miloš Hampl <miloshampl@hotmail.cz>
2018-07-23 19:05:15 +0000

    Merged PR 59: smoke session validation
    smoke session validation
    
    Related work items: #86

---
+++ smartHookah/Controllers/Api/SmokeSessionController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.TeamFoundation.Client.Reporting;
+using smartHookah.Models;
+using smartHookah.Models.Dto;
+using smartHookahCommon;
+
+namespace smartHookah.Controllers.Api
+{
+    [System.Web.Http.RoutePrefix("api/SmokeSession")]
+    public class SmokeSessionController : ApiController
+    {
+        private readonly SmartHookahContext _db;
+
+        public SmokeSessionController(SmartHookahContext db)
+        {
+            this._db = db;
+        }
+
+        #region Getters and Validators
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("Validate")]
+        public async Task<ValidationDTO> Validate(string smokeSessionId)
+        {
+            smokeSessionId = smokeSessionId.ToUpper();
+            
+            if (smokeSessionId.Length != 5)
+                return new ValidationDTO() { Success = false, Message = "Session id is not valid." };
+
+            var redisSessionId = RedisHelper.GetHookahId(smokeSessionId);
+            var dbSession = _db.SmokeSessions.Find(smokeSessionId);
+
+            if (dbSession == null)
+                return new ValidationDTO() { Success = false, Message = "Session id is not valid." };
+
+            if (redisSessionId == null && dbSession.Statistics != null)
+                return new ValidationDTO()
+                {
+                    Success = true,
+                    Message = $"Session with id {smokeSessionId} is not live.",
+                    Id = smokeSessionId,
+                    Flag = SessionState.Completed
+                };
+
+            if (redisSessionId != null && dbSession.Statistics == null)
+                return new ValidationDTO()
+                {
+                    Success = true,
+                    Message = $"Session with id {redisSessionId} is live.",
+                    Id = redisSessionId,
+                    Flag = SessionState.Live
+                };
+
+            return new ValidationDTO() { Success = false, Message = "Session not found." };
+        }
+
+        #endregion
+
+    }
+}
---
+++ smartHookah/Models/Dto/SmokeSessionDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace smartHookah.Models.Dto
+{
+    public class SmokeSessionDTO
+    {
+    }
+
+    public class ValidationDTO : DTO
+    {
+        public String Id { get; set; }
+        public SessionState Flag { get; set; }
+    }
+
+    public enum SessionState{ Live, Completed }
+}
