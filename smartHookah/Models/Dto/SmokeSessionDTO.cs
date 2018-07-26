aa31bcab1934f78a7ab0484a70eb89be035a094e
Miloš Hampl <miloshampl@hotmail.cz>
2018-07-26 17:22:43 +0000

    Merged PR 62: init data service, swagger fix
    Related work items: #88

--- smartHookah/Models/Dto/SmokeSessionDTO.cs
+++ smartHookah/Models/Dto/SmokeSessionDTO.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using smartHookah.Models.Redis;
 
 namespace smartHookah.Models.Dto
 {
@@ -16,4 +17,53 @@ namespace smartHookah.Models.Dto
     }
 
     public enum SessionState{ Live, Completed }
+
+    public class InitDataDTO : DTO
+    {
+        public DynamicSmokeStatistic RedisStatistics { get; set; }
+        public SmokeSessionStatistics SessionStatistics { get; set; }
+        public SmokeSessionMetaData SessionMetaData { get; set; }
+        public StandSettings StandSettings { get; set; }
+    }
+
+    public class StandSettings
+    {
+        public ActionSettings PuffSettings { get; set; }
+        public ActionSettings BlowSettings { get; set; }
+        public ActionSettings IdleSettings { get; set; }
+
+        public StandSettings(HookahSetting settings)
+        {
+            PuffSettings = new ActionSettings()
+            {
+                Color = settings.Color,
+                AnimationId = settings.PufAnimation,
+                Brightness = settings.PufBrightness,
+                Speed = settings.PufSpeed
+            };
+            BlowSettings = new ActionSettings()
+            {
+                Color = settings.Color,
+                AnimationId = settings.BlowAnimation,
+                Brightness = settings.PufBrightness,
+                Speed = settings.PufSpeed
+            };
+            IdleSettings = new ActionSettings()
+            {
+                Color = settings.Color,
+                AnimationId = settings.IdleAnimation,
+                Brightness = settings.IdleBrightness,
+                Speed = settings.IdleSpeed
+            };
+        }
+    }
+
+    public class ActionSettings
+    {
+        public Color Color { get; set; }
+        public int AnimationId { get; set; }
+        public int Brightness { get; set; }
+        public int Speed { get; set; }
+    }
+
 }
---
+++ smartHookah/Services/SmokeSession/IInitDataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using smartHookah.Models;
+using smartHookah.Models.Redis;
+
+namespace smartHookah.Services.SmokeSession
+{
+    interface IInitDataService
+    {
+        DynamicSmokeStatistic GetRedisData(string id);
+        SmokeSessionStatistics GetStatistics(string id);
+        SmokeSessionMetaData GetMetaData(string id);
+        HookahSetting GetStandSettings(string id);
+    }
+}
