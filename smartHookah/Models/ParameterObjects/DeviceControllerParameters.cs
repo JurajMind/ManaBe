ac93247a258d7e21f8247483458d9a7bbc9f63c7
Miloš Hampl <miloshampl@hotmail.cz>
2018-08-02 11:19:49 +0000

    Merged PR 68: device controls api
    Related work items: #93

---
+++ smartHookah/Models/ParameterObjects/DeviceControllerParameters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace smartHookah.Models.ParameterObjects
+{
+    public class ChangeAnimation
+    {
+        public int AnimationId { get; set; }
+        public PufType Type { get; set; }
+    }
+
+    public class ChangeSpeed
+    {
+        [Range(0,255)]
+        public int Speed { get; set; }
+        public PufType Type { get; set; }
+    }
+
+    public class ChangeColor
+    {
+        public Color Color { get; set; }
+        public PufType Type { get; set; }
+    }
+
+    public class ChangeBrightness
+    {
+        [Range(0, 255)]
+        public int Brightness { get; set; }
+        public PufType Type { get; set; }
+    }
+}
--- smartHookah/Services/Device/DeviceService.cs
+++ smartHookah/Services/Device/DeviceService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace smartHookah.Services.Device
 {
@@ -31,14 +32,89 @@ namespace smartHookah.Services.Device
             if (hookah == null) throw new ItemNotFoundException($"Device with id {deviceId} not found");
 
             if (animation.VersionFrom != 0 && animation.VersionTo != 0)
-              if (animation.VersionFrom <= hookah.Version && animation.VersionTo >= hookah.Version) return;
+              if (animation.VersionFrom <= hookah.Version && animation.VersionTo >= hookah.Version)
+                    throw new NotSupportedException($"Animation {animation.DisplayName} not supported by your Hookah OS version.");
 
-            var msg = $"led:{(int)state}{animation.Id}";
-            var sendTask = this.iotService.SendMsgToDevice(deviceId, msg);
+            var sendTask = this.iotService.SendMsgToDevice(deviceId, $"led:{(int)state}{animation.Id}");
 
             this.SetAnimation(hookah.Setting, (int)state, animation.Id);
             this.db.HookahSettings.AddOrUpdate(hookah.Setting);
             await Task.WhenAll(this.db.SaveChangesAsync(), sendTask);
+        }
+
+        public async Task SetBrightness(string deviceId, int brightness, PufType state)
+        {
+            var hookah = this.db.Hookahs.FirstOrDefault(a => a.Code == deviceId);
+
+            if (hookah == null) throw new ItemNotFoundException($"Device with id {deviceId} not found");
+
+            var sendTask = this.iotService.SendMsgToDevice(deviceId, $"br:{(int)state}{brightness}");
+
+            this.SetBrightness(hookah.Setting, (int)state, brightness);
+            this.db.HookahSettings.AddOrUpdate(hookah.Setting);
+            await Task.WhenAll(this.db.SaveChangesAsync(), sendTask);
+        }
+
+        public async Task SetSpeed(string deviceId, int speed, PufType state)
+        {
+            var hookah = this.db.Hookahs.FirstOrDefault(a => a.Code == deviceId);
+
+            if (hookah == null) throw new ItemNotFoundException($"Device with id {deviceId} not found");
+
+            var sendTask = this.iotService.SendMsgToDevice(deviceId, $"spd:{(int)state}{speed}");
+
+            this.SetSpeed(hookah.Setting, (int)state, speed);
+            this.db.HookahSettings.AddOrUpdate(hookah.Setting);
+            await Task.WhenAll(this.db.SaveChangesAsync(), sendTask);
+        }
+
+        public async Task SetColor(string deviceId, Color color, PufType state)
+        {
+            var hookah = this.db.Hookahs.FirstOrDefault(a => a.Code == deviceId);
+
+            if (hookah == null) throw new ItemNotFoundException($"Device with id {deviceId} not found");
+
+            var sendTask = this.iotService.SendMsgToDevice(deviceId, $"clr:{color.Hue:000},{color.Saturation:000},{color.Value:000}");
+
+            this.SetColor(hookah.Setting, (int)state, color);
+            this.db.HookahSettings.AddOrUpdate(hookah.Setting);
+            await Task.WhenAll(this.db.SaveChangesAsync(), sendTask);
+        }
+
+        public async Task Sleep(string deviceId)
+        {
+            var hookah = this.db.Hookahs.FirstOrDefault(a => a.Code == deviceId);
+
+            if (hookah == null) throw new ItemNotFoundException($"Device with id {deviceId} not found");
+
+            await this.iotService.SendMsgToDevice(deviceId, "slp:");
+        }
+
+        public async Task Restart(string deviceId)
+        {
+            var hookah = this.db.Hookahs.FirstOrDefault(a => a.Code == deviceId);
+
+            if (hookah == null) throw new ItemNotFoundException($"Device with id {deviceId} not found");
+
+            await this.iotService.SendMsgToDevice(deviceId, "restart:");
+        }
+
+        public async Task SetMode(string deviceId, int mode)
+        {
+            var hookah = this.db.Hookahs.FirstOrDefault(a => a.Code == deviceId);
+
+            if (hookah == null) throw new ItemNotFoundException($"Device with id {deviceId} not found");
+
+            await this.iotService.SendMsgToDevice(deviceId, $"mode:{mode}");
+        }
+
+        public async Task ShowQrCode(string deviceId)
+        {
+            var hookah = this.db.Hookahs.FirstOrDefault(a => a.Code == deviceId);
+
+            if (hookah == null) throw new ItemNotFoundException($"Device with id {deviceId} not found");
+
+            await this.iotService.SendMsgToDevice(deviceId, "qrcode:");
         }
 
         public Task<Dictionary<string, bool>> GetOnlineStates(IEnumerable<string> deviceIds)
@@ -49,6 +125,15 @@ namespace smartHookah.Services.Device
         public List<Animation> GetAnimations()
         {
             return AnimationHelper.Animations;
+        }
+
+        public Animation GetAnimation(int id)
+        {
+            var animations = AnimationHelper.Animations;
+            if (animations.Any(a => a.Id == id))
+                return animations.FirstOrDefault(a => a.Id == id);
+
+            throw new ItemNotFoundException("Animation not found.");
         }
 
         public int GetDeviceVersion(string id)
@@ -73,6 +158,48 @@ namespace smartHookah.Services.Device
                     setting.BlowAnimation = value;
                     return;
             }
+        }
+
+        private void SetBrightness(HookahSetting setting, int state, int value)
+        {
+            switch (state)
+            {
+                case 0:
+                    setting.IdleBrightness = value;
+                    return;
+
+                case 1:
+                    setting.PufBrightness = value;
+                    return;
+
+                case 2:
+                    setting.PufBrightness = value;
+                    return;
+            }
+        }
+
+        private void SetSpeed(HookahSetting setting, int state, int value)
+        {
+            switch (state)
+            {
+                case 0:
+                    setting.IdleSpeed = value;
+                    return;
+
+                case 1:
+                    setting.PufSpeed = value;
+                    return;
+
+                case 2:
+                    setting.PufSpeed = value;
+                    return;
+            }
+        }
+
+        private void SetColor(HookahSetting setting, int state, Color color)
+        {
+            //TODO color change for other states
+            setting.Color = color;
         }
     }
 }
