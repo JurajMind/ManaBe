879fcce73e6f7125cd0911845e4b5f59de5f216d
Miloš Hampl <miloshampl@hotmail.cz>
2018-08-02 11:19:49 +0000

    Merged PR 68: device controls api
    Related work items: #93

--- smartHookah/Services/Device/IDeviceService.cs
+++ smartHookah/Services/Device/IDeviceService.cs
@@ -11,7 +11,15 @@ namespace smartHookah.Services.Device
     public interface IDeviceService
     {
         Task SetAnimation(string deviceId, Animation animation, PufType state);
+        Task SetBrightness(string deviceId, int brightness, PufType state);
+        Task SetSpeed(string deviceId, int speed, PufType state);
+        Task SetColor(string deviceId, Color color, PufType state);
+        Task Sleep(string deviceId);
+        Task Restart(string deviceId);
+        Task SetMode(string deviceId, int mode);
+        Task ShowQrCode(string deviceId);
         List<Animation> GetAnimations();
+        Animation GetAnimation(int id);
         int GetDeviceVersion(string id);
 
         Task<Dictionary<string, bool>> GetOnlineStates(IEnumerable<string> deviceIds);
