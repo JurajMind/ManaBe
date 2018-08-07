8f65a6e7744f6b476d1fdf6ffc7b844b87c0ecc1
Miloš Hampl <miloshampl@hotmail.cz>
2018-08-07 16:22:44 +0200

    get my gear

---
+++ smartHookah/Services/Gear/IGearService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using smartHookah.Models;
+
+namespace smartHookah.Services.Gear
+{
+    public interface IGearService
+    {
+        List<PipeAccesory> GetPersonAccessories(int personId);
+    }
+}
