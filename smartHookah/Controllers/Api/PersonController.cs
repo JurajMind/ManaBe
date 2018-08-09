100272c3f49e159158e0bf3fca77a4621e0e24c5
Miloš Hampl <miloshampl@hotmail.cz>
2018-08-09 13:48:17 +0200

    get my gear, vote fix

--- smartHookah/Controllers/Api/PersonController.cs
+++ smartHookah/Controllers/Api/PersonController.cs
@@ -18,13 +18,13 @@ namespace smartHookah.Controllers.Api
     [RoutePrefix("api/Person")]
     public class PersonController : ApiController
     {
-        private readonly IPersonService _personService;
-        private readonly IGearService _gearService;
+        private readonly IPersonService personService;
+        private readonly IGearService gearService;
 
         public PersonController(IPersonService personService, IGearService gearService)
         {
-            this._personService = personService;
-            _gearService = gearService;
+            this.personService = personService;
+            this.gearService = gearService;
         }
 
         #region Getters
@@ -33,7 +33,7 @@ namespace smartHookah.Controllers.Api
         [Route("GetPersonActiveData")]
         public async Task<PersonActiveDataDTO> GetPersonActiveData()
         {
-            var person = this._personService.GetCurentPerson();
+            var person = this.personService.GetCurentPerson();
 
             if (person == null)
             {
@@ -42,15 +42,15 @@ namespace smartHookah.Controllers.Api
             }
 
             var personId = person.Id;
-            var standTask = await this._personService.GetUserActiveStands(personId);
+            var standTask = await this.personService.GetUserActiveStands(personId);
             var stands = standTask.Select(HookahSimpleDto.FromModel).ToList();
-            var sessions = this._personService.GetUserActiveSessions(personId)
+            var sessions = this.personService.GetUserActiveSessions(personId)
                 .Select(SmokeSessionSimpleDto.FromModel)
                 .ToList();
-            var reservations = this._personService.GetUserActiveReservations(personId)
+            var reservations = this.personService.GetUserActiveReservations(personId)
                 .Select(Models.Dto.ReservationDto.FromModel).ToList();
-            var orders = this._personService.GetUserHookahOrders(personId).Select(HookahOrderDto.FromModel).ToList();
-            var gameProfile = GameProfileSimpleDto.FromModel(this._personService.GetUserGameProfile(personId));
+            var orders = this.personService.GetUserHookahOrders(personId).Select(HookahOrderDto.FromModel).ToList();
+            var gameProfile = GameProfileSimpleDto.FromModel(this.personService.GetUserGameProfile(personId));
 
             return new PersonActiveDataDTO()
             {
@@ -63,16 +63,16 @@ namespace smartHookah.Controllers.Api
         }
 
         [HttpGet, Authorize, Route("MyGear")]
-        public IEnumerable<PipeAccesorySimpleDto> MyGear()
+        public IEnumerable<PipeAccesorySimpleDto> MyGear(string type = "All")
         {
-            var person = this._personService.GetCurentPerson();
+            var person = this.personService.GetCurentPerson();
 
             if (person == null)
             {
                 throw new HttpResponseException(
                     this.Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Person not logged"));
             }
-            var accessories = _gearService.GetPersonAccessories(person.Id);
+            var accessories = gearService.GetPersonAccessories(person.Id, type);
             foreach (var acc in accessories)
             {
                 yield return PipeAccesorySimpleDto.FromModel(acc);
