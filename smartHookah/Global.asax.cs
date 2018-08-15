39ae99703b19e7d51a93c671f8b2ba8b84ea3561
Juraj Hamornik <hamornik@manapipes.com>
2018-08-15 22:26:32 +0200

    dynamic ip restriction

--- smartHookah/Global.asax.cs
+++ smartHookah/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿namespace smartHookah
 {
+    using System;
     using System.Reflection;
     using System.Web;
     using System.Web.Http;
@@ -44,6 +45,49 @@
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
             config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            if (Context.IsCustomErrorEnabled)
+                ShowCustomErrorPage(Server.GetLastError());
+        }
+
+        private void ShowCustomErrorPage(Exception exception)
+        {
+            HttpException httpException = exception as HttpException;
+            if (httpException == null)
+                httpException = new HttpException(500, "Internal Server Error", exception);
+
+            Response.Clear();
+            RouteData routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            routeData.Values.Add("fromAppErrorEvent", true);
+
+            switch (httpException.GetHttpCode())
+            {
+                case 403:
+                    routeData.Values.Add("action", "AccessDenied");
+                    break;
+
+                case 404:
+                    routeData.Values.Add("action", "NotFound");
+                    break;
+
+                case 500:
+                    routeData.Values.Add("action", "ServerError");
+                    break;
+
+                default:
+                    routeData.Values.Add("action", "OtherHttpStatusCode");
+                    routeData.Values.Add("httpStatusCode", httpException.GetHttpCode());
+                    break;
+            }
+
+            Server.ClearError();
+
+            IController controller = new ErrorController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
         }
     }
 }
