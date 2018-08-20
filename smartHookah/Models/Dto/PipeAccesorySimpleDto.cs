ab8349bd54cf8d101148c1abf03378c1a7087a7b
Miloš Hampl <miloshampl@hotmail.cz>
2018-08-20 17:18:19 +0200

    authorize attribute

--- smartHookah/Models/Dto/PipeAccesorySimpleDto.cs
+++ smartHookah/Models/Dto/PipeAccesorySimpleDto.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
-using Microsoft.AspNet.SignalR;
+using System.Web.Http;
 using Newtonsoft.Json;
 
 namespace smartHookah.Models.Dto
