8f282951b950ba178903f0afc8ec70ad0a63b55a
Juraj Hamornik <jurko@bdi.sk>
2018-10-04 16:53:23 +0200

    fix

--- smartHookah/App_Start/IdentityConfig.cs
+++ smartHookah/App_Start/IdentityConfig.cs
@@ -1,15 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.IO;
-using System.Linq;
 using System.Net.Mail;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Web;
-using System.Web.Hosting;
-using log4net;
-using Mailzory;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
@@ -19,76 +12,6 @@ using smartHookah.Models;
 
 namespace smartHookah
 {
-    public class EmailService : IEmailService, IIdentityMessageService
-    {
-        private readonly ILog logger = LogManager.GetLogger(typeof(EmailService));
-        public void SendTemplateAsync(string mailAdress,string subject,string template,object model)
-        {
-            if(mailAdress == null)
-            {
-                return;
-            }
-
-            // template path
-            var viewPath = Path.Combine("~/Views/Emails", template);
-            viewPath = HostingEnvironment.MapPath(viewPath);
-            // read the content of template and pass it to the Email constructor
-            var layoutPath = HostingEnvironment.MapPath("~/Views/Emails/_EmailLayout.cshtmlTemplate");
-            var layout = File.ReadAllLines(layoutPath);
-
-            var index = Array.FindIndex(layout,a => a.Contains("#BODY#"));
-
-            var first = layout.Take(index);
-            var seccond = layout.Skip(index + 1);
-                          
-            var readedTemplate = File.ReadLines(viewPath);
-
-            var compile = first.Concat(readedTemplate).Concat(seccond);
-         
-            var email = new Email(string.Join("", compile));
-            email.ViewBag.Model = model;
-            email.SetFrom("app@manapipes.com", "App smoke-o-bot");
-            HostingEnvironment.QueueBackgroundWorkItem(async ct =>
-            {
-                try
-                {
-                  await email.SendAsync(mailAdress, subject);
-                }
-                catch (Exception ex)
-                {
-                    logger.Error(ex);
-                }
-            });
-
-          
-        }
-
-        public Task SendAsync(IdentityMessage message)
-        {
-            // template path
-            var viewPath = Path.Combine("~/Views/Emails", "hello.cshtml");
-            viewPath = HttpContext.Current.Server.MapPath(viewPath);
-            // read the content of template and pass it to the Email constructor
-            var template = File.ReadAllText(viewPath);
-
-            var email = new Email(message.Body);
-
-
-         
-         
-            // set Attachments (Optional)
-            //email.Attachments.Add(new Attachment("Attachments/attach1.pdf"));
-            //email.Attachments.Add(new Attachment("Attachments/attach2.docx"));
-
-            // set your desired display name (Optional)
-            email.SetFrom("app@manapipes.com", "App smoke-o-bot");
-
-            // send email with BCC
-            return email.SendAsync(message.Destination,message.Subject);
-
-
-        }
-    }
 
     public class SmsService : IIdentityMessageService
     {
