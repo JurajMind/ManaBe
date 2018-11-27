58c49b59839d7ec40994c6e13deb04289938a734
Miloš Hampl <miloshampl@hotmail.cz>
2018-11-27 00:34:30 +0000

    Merged PR 113: return device preset on save
    return device preset on save
    
    Related work items: #117

--- smartHookah/Services/Device/IDeviceSettingsPresetService.cs
+++ smartHookah/Services/Device/IDeviceSettingsPresetService.cs
@@ -8,16 +8,23 @@ namespace smartHookah.Services.Person
     public interface IDeviceSettingsPresetService
     {
         bool SetDefault(int presetId);
+
         bool SetDefault(string sessionCode);
-        int AddPreset(string name, DeviceSetting setting);
-        int SaveSessionPreset(string sessionCode, string name = "", bool addToPerson = true);
-        int SaveDevicePreset(string deviceId, string name = "", bool addToPerson = true);
+
+        DevicePreset AddPreset(string name, DeviceSetting setting);
+
+        DevicePreset SaveSessionPreset(string sessionCode, string name = "", bool addToPerson = true);
+
+        DevicePreset SaveDevicePreset(string deviceId, string name = "", bool addToPerson = true);
+
         ICollection<DevicePreset> GetUserPresets();
+
         Task<DevicePreset> GetPreset(int id);
 
         Task<IList<DevicePreset>> GetPresets();
 
         Task Delete(int id);
+
         Task<bool> UseDefaut(string id);
 
         Task<bool> UsePreset(string deviceId, int presetId);
