ddce234bbe770f2772d08d5e352c07b9b776ac5a
Juraj Hamornik <juraj.hamornik@outlook.com>
2018-11-30 19:34:21 +0000

    Merged PR 119: device api fix
    Related work items: #101, #103, #104, #106, #108, #109, #114, #116, #117, #119, #43, #53, #58, #62, #65, #66, #69, #73, #75, #76, #78, #79, #86, #87, #88, #89, #93, #99

---
+++ smartHookah/Models/Dto/PipeAccessoryUsageDto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace smartHookah.Models.Dto
+{
+    [DataContract]
+    public class PipeAccessoryUsageDto
+    {
+        [DataMember, JsonProperty("Id")]
+        public int Id { get; set; }
+
+        [DataMember, JsonProperty("BrandName")]
+        public string BrandName { get; set; }
+
+        [DataMember, JsonProperty("AccName")]
+        public string AccName { get; set; }
+
+        [DataMember, JsonProperty("Type")]
+        public string Type { get; set; }
+
+        [DataMember, JsonProperty("Used")]
+        public int Used { get; set; }
+
+        public static PipeAccessoryUsageDto FromModel(PipeAccesory model) => model == null
+            ? null
+            : new PipeAccessoryUsageDto
+            {
+                Id = model.Id,
+                AccName = model.AccName,
+                Type = model.GetTypeName(),
+                Used = model.Statistics.Used,
+                BrandName = model.BrandName
+            };
+
+        public static IEnumerable<PipeAccessoryUsageDto> FromModelList(IEnumerable<PipeAccesory> model)
+        {
+            if (model == null) yield break;
+            foreach (var item in model)
+            {
+                yield return FromModel(item);
+            }
+        } 
+    }
+}
---
+++ smartHookah/Models/Dto/SmokeSessionTimeStatisticsDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace smartHookah.Models.Dto
+{
+    [DataContract]
+    public class SmokeSessionTimeStatisticsDto
+    {
+        [DataMember, JsonProperty("SessionsCount")]
+        public int SessionsCount { get; set; }
+
+        [DataMember, JsonProperty("SessionsDuration")]
+        public TimeSpan SessionsDuration { get; set; }
+
+        [DataMember, JsonProperty("SmokeDuration")]
+        public TimeSpan SmokeDuration { get; set; }
+
+        [DataMember, JsonProperty("PuffCount")]
+        public int PuffCount { get; set; }
+
+        [DataMember, JsonProperty("DayOfWeekDistribution")]
+        public Dictionary<int, int> DayOfWeekDistribution { get; set; }
+
+        [DataMember, JsonProperty("SessionStartTimeDistribution")]
+        public Dictionary<int, int> SessionStartTimeDistribution { get; set; }
+    }
+}
