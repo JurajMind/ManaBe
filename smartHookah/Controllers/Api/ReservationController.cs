7ca1b0e44695d0bf984866f9898a68c2dfd3550c
Juraj Hamornik <hamornik@manapipes.com>
2019-01-07 02:16:43 +0100

    placeFix

---
+++ smartHookah/Controllers/Api/ReservationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Http;
+
+namespace smartHookah.Controllers.Api
+{
+    using System.Threading.Tasks;
+
+    using smartHookah.Models.Dto;
+    using smartHookah.Services.Place;
+
+    [RoutePrefix("api/Reservations")]
+    public class ReservationController : ApiController
+    {
+        private readonly IReservationService reservationService;
+        
+        public ReservationController(IReservationService reservationService)
+        {
+            this.reservationService = reservationService;
+        }
+
+        [HttpGet, Route("{id}/Manage")]
+        public async Task<ReservationManageDto> GetManagereservationsInfo(int id,DateTime date)
+        {
+            return await this.reservationService.GetReservationManage(id, date);
+        }
+
+    }
+}
