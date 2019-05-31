30e7391386054672382498552d18e91f5f15b756
Juraj Hamornik <hamornik@manapipes.com>
2019-05-31 22:34:34 +0200

    competiotion

---
+++ smartHookah/Models/Redis/CompetitionEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace smartHookah.Models.Redis
+{
+    public class CompetitionEntry
+    {
+        public string Name { get; set; }
+
+        public double Time { get; set; }
+    }
+}
