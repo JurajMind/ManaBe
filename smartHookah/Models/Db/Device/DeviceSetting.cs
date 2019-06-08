ca67cb421e8b254e7b50a7b09803559c715e440e
Juraj Hamornik <hamornik@manapipes.com>
2019-06-08 16:54:39 +0200

    multiple color

--- smartHookah/Models/Db/Device/DeviceSetting.cs
+++ smartHookah/Models/Db/Device/DeviceSetting.cs
@@ -152,6 +152,16 @@
             this.PufSpeed = defaultAnimation.PufSpeed;
             this.PufBrightness = defaultAnimation.PufBrightness;
         }
+
+        public string GetInitMultipleColor(int intake, int percentage, string sessionId)
+        {
+            return $"{PufAnimation},{BlowAnimation},{IdleAnimation},{intake},{percentage}" +
+                   $",{Color.Hue:000},{Color.Saturation:000},{Color.Value:000}," +
+                   $",{Color.Hue:000},{Color.Saturation:000},{Color.Value:000}," +
+                   $",{Color.Hue:000},{Color.Saturation:000},{Color.Value:000}," +
+                   $"{IdleBrightness},{PufBrightness},{GetBtStateInit()}," +
+                   $"{IdleSpeed},{PufSpeed},{sessionId},";
+        }
     }
 
     public enum BtState
