374981693cf0027ef1c34c227dbaf03c5d93efd3
Juraj Hamornik <hamornik@manapipes.com>
2019-06-11 20:34:54 +0200

    update service

--- smartHookah/Controllers/Api/DeviceController.cs
+++ smartHookah/Controllers/Api/DeviceController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using smartHookah.ErrorHandler;
 using smartHookah.Models.Db;
+using smartHookah.Models.Dto.Device;
 using smartHookah.Models.ParameterObjects;
 using smartHookah.Services.Device;
 using smartHookah.Services.Person;
@@ -13,6 +16,7 @@ namespace smartHookah.Controllers.Api
 {
     using smartHookah.Models.Dto;
 
+    [ApiAuthorize]
     [RoutePrefix("api/Device")]
     public class DeviceController : ApiController
     {
@@ -22,10 +26,13 @@ namespace smartHookah.Controllers.Api
 
         private readonly IPersonService personService;
 
-        public DeviceController(IDeviceService deviceService, IDeviceSettingsPresetService deviceSettingsPresetService)
+        private readonly IUpdateService updateService;
+
+        public DeviceController(IDeviceService deviceService, IDeviceSettingsPresetService deviceSettingsPresetService, IUpdateService updateService)
         {
             this.deviceService = deviceService;
             this.deviceSettingsPresetService = deviceSettingsPresetService;
+            this.updateService = updateService;
         }
 
         [HttpPost, Route("{id}/ChangeAnimation")]
@@ -200,8 +207,17 @@ namespace smartHookah.Controllers.Api
             return DeviceSimpleDto.FromModel(added);
         }
 
+        [HttpPost, Route("{id}/Update/{updateId}")]
+        public async Task<bool> PromptUpdate(int id, int updateId)
+        {
+           return await this.deviceService.UpdateDevice(id, updateId, this.personService.GetCurentPerson(), User.IsInRole("Admin"));
+        }
 
-
-
+        [HttpGet, Route("Updates")]
+        public async Task<ICollection<UpdateDto>> Updates()
+        {
+            var updates = await this.updateService.GetUpdates();
+            return updates.Select(UpdateDto.FromModel).ToList();
+        }
     }
 }
---
+++ smartHookah/Services/Device/IUpdateService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using smartHookah.Models.Db;
+
+namespace smartHookah.Services.Device
+{
+    public interface IUpdateService
+    {
+        Task<ICollection<Update>> GetUpdates();
+    }
+}
